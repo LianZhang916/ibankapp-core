a80608d89343381be9b1709cdb9825b6295ae7a7
codelder <wangyue@ibankapp.org>
2016-10-19 22:42:16 +0800

    update copyright

--- src/main/java/org/ibankapp/core/type/Idtp.java
+++ src/main/java/org/ibankapp/core/type/Idtp.java
@@ -1,3 +1,12 @@
+/*
+ * iBankApp
+ *
+ * License : Apache License,Version 2.0, January 2004
+ *
+ * See the LICENSE file in English or LICENSE.zh_CN in chinese
+ * in the root directory or <http://www.apache.org/licenses/>.
+ */
+
 package org.ibankapp.core.type;
 
 /**
